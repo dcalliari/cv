abcb267022f7e57f39815d313ad974bf1653baf7
daniel calliari <booude@gmail.com>
2025-01-30 04:01:40 -0300

    Update resume-pt.ts

--- src/data/resume-pt.ts
+++ src/data/resume-pt.ts
@@ -24,7 +24,7 @@ export const ptResume: Resume = {
 		japanese: "Japonês",
 	},
 	contact: {
-		email: "dbcalliari@gmail.com",
+		email: "daniel@calliari.dev",
 		tel: "+55 91 9 8209-8271",
 		social: [
 			{
@@ -50,7 +50,7 @@ export const ptResume: Resume = {
 			school: "Universidade Federal Rural da Amazônia",
 			degree: "Bacharelado em Sistemas de Informação, transferido",
 			start: "2018",
-			end: "2020",
+			end: "2022",
 		},
 	],
 	work: [
